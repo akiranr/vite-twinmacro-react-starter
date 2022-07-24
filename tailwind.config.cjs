3ae86e5b9d5b2f3f5262e43559fd0cf91e0fe569
kiran <aryaenterprises.cloud@gmail.com>
2022-07-24 18:28:52 +1000

    starter commit

---
+++ tailwind.config.cjs
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {
+      colors: {
+        rosewood: {
+          50: "#ffeeee",
+          100: "#ffdada",
+          200: "#ffbbbb",
+          300: "#ff8b8b",
+          400: "#ff4949",
+          500: "#ff1111",
+          600: "#ff0000",
+          700: "#e70000",
+          800: "#be0000",
+          900: "#620000",
+          light: "#914d4d",
+        },
+      },
+    },
+  },
+  plugins: [],
+};
